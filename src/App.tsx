273d9970d3d2e4f1343623fb5a23958924d60fff
Juliano Reis <juliano@nave.rs>
2020-05-17 13:04:31 -0300

    feat(styled-components): setting up styled-components theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+import { theme } from 'theme'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+  button, a {
+    cursor: pointer;
+    &:disabled{
+      cursor: not-allowed;
+    }
+  }
+`
 
 const App: React.FC = () => {
-  return <p>React boilerplate typescript</p>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <p>React boilerplate typescript</p>
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components'
+
+const theme: DefaultTheme = {
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+    blue: 'blue'
+  }
+}
+
+export default theme
