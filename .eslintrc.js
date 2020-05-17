fafe0d108e3d6cde071abf044bcc7b44a2f6c8e5
Juliano Reis <juliano@nave.rs>
2020-05-16 23:09:10 -0300

    feat(init): initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['react-hooks'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    quotes: [2, 'single'],
+    semi: [2, 'never'],
+    'comma-dangle': [2, 'never'],
+    'no-trailing-spaces': [2],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/jsx-boolean-value': [0],
+    'react-hooks/rules-of-hooks': 'error',
+    'prettier/prettier': 2,
+    'space-before-function-paren': 0,
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false
+}
