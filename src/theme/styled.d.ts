df2b500fbebe996886995f1fe9f88b9af7ec4db5
Lucas Barbosa <lucasbarbosa@Lucass-MacBook-Pro.local>
2020-05-31 21:05:16 -0300

    feat(project): initial setup

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -3,6 +3,12 @@ import 'styled-components'
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
+      black: string
+      white: string
+      blue400: string
+      green300: string
+      green200: string
+      blue100: string
       primary: string
       secondary: string
       terciary: string
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,6 +2,12 @@ import { DefaultTheme } from 'styled-components'
 
 const theme: DefaultTheme = {
   colors: {
+    black: '#000',
+    white: '#fff',
+    blue400: '#190b2a',
+    green300: '#219963',
+    green200: '#64bba7',
+    blue100: '#00ddff',
     primary: '#0D1321',
     secondary: '#FFEDDF',
     terciary: '#C5D86D',
